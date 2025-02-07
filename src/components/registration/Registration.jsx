7975e45f11193c885c298efe482623cca09a8696
Arpita <arpitasd13@gmail.com>
2025-02-07 10:52:26 +0530

    registration form is edited

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -3,36 +3,47 @@ import './Registration.css'
 
 const Registration = () => {
   return (
-    <div className="wrapper">
-    <h2>Registration</h2>
-    <form action="#">
-    <div className="input-box">
-        <input type="text" placeholder="Enter employee ID" required/>
-      </div>
-      <div className="input-box">
-        <input type="text" placeholder="Enter your name" required/>
-      </div>
-      <div className="input-box">
-        <input type="text" placeholder="Enter your email" required/>
-      </div>
-      <div className="input-box">
-        <input type="password" placeholder="Create password" required/>
-      </div>
-      <div className="input-box">
-        <input type="password" placeholder="Confirm password" required/>
-      </div>
-      <div className="policy">
-        <input type="checkbox"/>
-        <h3>I accept all terms & condition</h3>
-      </div>
-      <div className="input-box button">
-        <input type="Submit" value="Register Now"/>
-      </div>
-      <div className="text">
-        <h3>Already have an account? <a href="#">Login now</a></h3>
-      </div>
-    </form>
-  </div>)
+<div className="wrapper">
+      <h2>Registration Form</h2>
+      <form >
+        <div className="input-box">
+          <input type="text" name="employeeId" placeholder="Enter Employee ID"/>
+        </div>
+        <div className="input-box">
+          <input type="text" name="firstName" placeholder="Enter First Name" />
+        </div>
+        <div className="input-box">
+          <input type="text" name="lastName" placeholder="Enter Last Name"/>
+        </div>
+        <div className="input-box">
+          <input type="email" name="email" placeholder="Enter Your Email" />
+        </div>
+        <div className="input-box">
+          <input type="password" name="password" placeholder="Create Password"/>
+        </div>
+        <div className="input-box">
+          <input type="password" name="confirmPassword" placeholder="Confirm Password" />
+        </div>
+        <div className="input-box">
+          <input type="text" />
+          <select name="role">
+            <option value="Manager">Manager</option>
+            <option value="HR">HR</option>
+          </select>
+        </div>
+        <div className="policy">
+          <input type="checkbox" name="acceptedTerms"  />
+          <h3>I accept all terms & conditions</h3>
+        </div>
+        <div className="input-box button">
+          <input type="submit" value="Register Now" />
+        </div>
+        <div className="text">
+          <h3>Already have an account? <a href="#">Login now</a></h3>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Registration
